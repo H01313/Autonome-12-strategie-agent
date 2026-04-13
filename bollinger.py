94f76e13e70464e538447fb87eb6918f40a532b9
H01313 <office@eightgroup.nl>
2026-04-13 12:14:22 +0200

    Delete bollinger.py

--- bollinger.py
+++
@@ -1,8 +0,0 @@
-def bollinger_bands(df, period=20):
-    sma = df['close'].rolling(period).mean()
-    std = df['close'].rolling(period).std()
-
-    upper = sma + 2 * std
-    lower = sma - 2 * std
-
-    return upper, lower
